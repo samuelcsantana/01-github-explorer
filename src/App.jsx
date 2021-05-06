e5005bd80937c6e3e8dccb0c91fb56f01dae6363
Samuel Santana <samuel.ssa89@gmail.com>
2021-05-05 22:03:21 -0300

    add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
-export function App() {
-
-  //throw new Error('Eita Giovana, o forninho caiu!');
-
-  
-    return <h1>Hello RactJS</h1>
+export function App() { 
+    return <RepositoryList/>
 }
